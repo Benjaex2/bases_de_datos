11feea2f612b16cdf46eddb2caeeaa835902c5de
Benjaex2 <benjaex2@gmail.com>
2018-07-13 07:59:57 -0400

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -1,50 +1,71 @@
-from configuraciones import *
 import psycopg2
-conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
+conn = psycopg2.connect("dbname=rut_199018795 user=postgres password=alumno")
+
 
 cur = conn.cursor()
 sql ="""DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;"""
 
 cur.execute(sql)
+sql ="""
+CREATE TABLE usuarios
+           (id serial PRIMARY KEY, nombre varchar(20), contrasena varchar(25), correo text not null unique, direccion varchar, creado timestamp DEFAULT NOW());
+"""
+cur.execute(sql)
+
 
 sql ="""
-CREATE TABLE posts 
-           (id serial PRIMARY KEY, titulo varchar(40), resumen varchar, texto text, creado timestamp);
+CREATE TABLE estados
+           (id_pedido integer PRIMARY KEY, estado varchar(15));
 """
+
+
+cur.execute(sql)
+sql ="""
+CREATE TABLE productos
+           (codigo integer PRIMARY KEY, nombre varchar, tipo varchar(10), precio integer, descuento integer, descripcion varchar);
+"""
+
+
+cur.execute(sql)
+
+sql ="""
+CREATE TABLE pedidos
+           (id serial PRIMARY KEY, id_usuario integer, monto integer, cod_producto integer, cantidad integer, talla varchar(3));
+"""
+
+
+cur.execute(sql)
+sql ="""
+CREATE TABLE inventario
+           (id_prod integer, talla varchar(2), cantidad integer);
+"""
+
+
+cur.execute(sql)
+sql ="""
+CREATE TABLE productos_pedidos
+           (productos_codigo integer, pedidos_id integer);
+"""
+
 
 cur.execute(sql)
 
 
 sql ="""
-CREATE TABLE categorias 
+CREATE TABLE categorias
            (id serial PRIMARY KEY, nombre varchar(40), creado timestamp);
-"""
 
+"""
 cur.execute(sql)
 
 sql ="""
-CREATE TABLE categorias_posts 
-           (categoria_id integer, post_id integer);
+CREATE TABLE categorias_productos 
+           (categoria_id integer, producto_id integer);
 """
 
-cur.execute(sql)
-
-sql ="""
-CREATE TABLE  usuarios
-           (id serial PRIMARY KEY,rol integer, nombre varchar(40),apellido varchar(40),
-           email varchar(100),passwd varchar(255), creado timestamp);
-"""
 
 cur.execute(sql)
-
-sql ="""
-CREATE TABLE comentarios
-           (id serial PRIMARY KEY, comentario varchar(140), post_id integer, usuario_id integer, creado timestamp);
-"""
-
-cur.execute(sql)
-
 
 conn.commit()
 cur.close()
